d9e17a4a5718451c4142c69c6f287a8e7d3f4386
DAVI EICHELBERGER <03193603007@senacrs.edu.br>
2025-06-09 21:13:02 -0300

    servidor ts v4

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import { usuariosRoutes } from "./routes/usuarios.routes"
 import { handleErrors } from "./errors"
 import { postsRoutes } from './routes/posts.routes'
 import { loginRoutes } from './routes/login.routes'
+
 const app:Application = express()
 
 app.use(express.json())
 app.use("/usuarios",usuariosRoutes)
 app.use("/posts",postsRoutes)
 app.use("/login",loginRoutes)
+
 
 app.use(handleErrors)
 export default app
--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -3,6 +3,8 @@ import { validateDataMiddleware } from "../middleware/validateData.middleware";
 import { craetePostsSchema } from "../schemas/posts.schemas";
 import { createPostController, getAllPostsController } from "../controllers/posts.controllers";
 import { validateTokenMiddleware } from "../middleware/validateToken.middleware";
+import { createCommentSchema } from "../schemas/comments.schemas";
+import { createCommentController, getCommentsByPostIdController } from "../controllers/comment.controllers";
 
 export const postsRoutes:Router = Router()
 
@@ -12,3 +14,6 @@ postsRoutes.delete("/:id",validateTokenMiddleware)
 postsRoutes.get("/user/:userid",)
 postsRoutes.get("/:id")
 postsRoutes.get("",getAllPostsController)
+
+postsRoutes.get("/comment/:id",getCommentsByPostIdController)
+postsRoutes.post("/comment/:id",validateTokenMiddleware,validateDataMiddleware(createCommentSchema),createCommentController)
